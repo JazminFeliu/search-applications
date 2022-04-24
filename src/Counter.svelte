d52179c5937f3aeaf9d4732314ff17df00861ee1
JazminFeliu <jazminfeliu@gmail.com>
2022-04-24 15:19:05 -0300

    subo declaraciones reactivas

--- src/Counter.svelte
+++ src/Counter.svelte
@@ -1,14 +1,22 @@
 <script>
   export let initialCounter = 0;
   let contador = initialCounter;
+  export let maxCounter = 9;
 
   function handleClick() {
     contador++;
+  }
+
+  $: isEvenOrOdd = contador % 2 === 0 ? "Is Even" : "Is Odd";
+  $: if (contador > maxCounter) {
+    console.log("limit contador");
+    contador = maxCounter;
   }
 </script>
 
 <button on:click={handleClick}>Incrementar</button>
 <span> {contador} </span>
+<span>{isEvenOrOdd} </span>
 
 <style>
   span {
---
+++ src/Input.svelte
@@ -0,0 +1,17 @@
+<script>
+  let value = "";
+  let response = [];
+
+  const handlerInput = (event) => (value = event.target.value);
+
+  $: if (value.length > 2) {
+    fetch(`http://www.omdbapi.com/?s=${value}&apikey=422350ff`)
+      .then((res) => res.json())
+      .then((apiResponse) => {
+        response = apiResponse.Search;
+        console.log(apiResponse);
+      });
+  }
+</script>
+
+<input {value} on:input={handlerInput} />
