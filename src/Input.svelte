a2cd2f4eccf2456a0c25c29712601e6855446a1b
JazminFeliu <jazminfeliu@gmail.com>
2022-04-25 19:59:18 -0300

    subo renderizado condicional

--- src/Input.svelte
+++ src/Input.svelte
@@ -1,17 +1,29 @@
 <script>
   let value = "";
+  let louding = false;
   let response = [];
 
-  const handlerInput = (event) => (value = event.target.value);
+  const handleInput = (event) => (value = event.target.value);
 
   $: if (value.length > 2) {
+    loading = true;
+
     fetch(`http://www.omdbapi.com/?s=${value}&apikey=422350ff`)
       .then((res) => res.json())
       .then((apiResponse) => {
-        response = apiResponse.Search;
-        console.log(apiResponse);
+        response = apiResponse.Search || [];
+
+        louding = false;
       });
   }
 </script>
 
-<input {value} on:input={handlerInput} />
+<input placeholder="Search movies..." {value} on:input={handleInput} />
+
+{#if loading}
+  <strong>Loading...</strong>
+{:else if response.length > 0}
+  <strong>Tenemos {response.length} películas</strong>
+{:else}
+  <strong>No hay resultados</strong>
+{/if}
